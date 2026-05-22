4b3e6b70803bea9b1c28622b2a567f333611f05c
Ubuntu <ubuntu@ip-172-31-45-73.ap-south-1.compute.internal>
2026-05-22 14:24:35 +0000

    Initial Terraform AWS project

---
+++ main.tf
@@ -0,0 +1,63 @@
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install apache2 -y
+              systemctl start apache2
+              systemctl enable apache2
+              echo "Hello Vaibhav from Terraform" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "terraform-server"
+  }
+}
